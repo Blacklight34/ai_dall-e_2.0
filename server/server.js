497ae47fa5ba4f16a9c8b4f6c53e15a44ebb95d0
Blacklight34 <sahilgthakur1@gmail.com>
2023-03-09 21:42:02 +0530

    added new file

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(cors())
 app.use(express.json({limit: '50MB'}))
 app.use("/api/v1/dalle", dalleRoutes)
-
+app.get('/', async (req, res) => {
+    res.status(200).json({
+      message: 'Hello from DALL.E!',
+    });
+  });
 
 app.listen(3000, ()=>{
     try{
