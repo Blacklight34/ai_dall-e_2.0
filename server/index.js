0777a06b76b6b6f76d4af1953a64997c4da513de
Blacklight34 <sahilgthakur1@gmail.com>
2023-03-09 21:15:17 +0530

    no db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
-import mongoose from 'mongoose'
 dotenv.config()
 import dalleRoutes from './routes/dalleRoutes.js'
 
@@ -11,21 +10,9 @@ app.use(cors())
 app.use(express.json({limit: '50MB'}))
 app.use("/api/v1/dalle", dalleRoutes)
 
-const connectMongo = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect("mongodb://127.0.0.1:27017/dalle",{useNewUrlParser: true})
-    const db = mongoose.connection;
-    db.on("error",(error)=>{
-        console.log(error)
-    })
-    db.once("connected",()=>{
-        console.log("Database Connected")
-    })
-}
 
 app.listen(3000, ()=>{
     try{
-        connectMongo()
         console.log("Port Listening on: " + 3000)
     }
     catch(err){
